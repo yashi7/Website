4a2e16663459a6267507e277e9e35c118728b588
Yashi <yashikaranwal03@gmail.com>
2023-12-06 19:34:16 +0530

    update

---
+++ home.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    var preloader = document.querySelector('.preloader');
+    setTimeout(function() {
+        preloader.style.display = 'none';
+    }, 2000);
+});
